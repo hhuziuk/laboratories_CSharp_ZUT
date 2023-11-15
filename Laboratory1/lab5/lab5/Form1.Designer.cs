3b808e919d641322ed0ee5d8140031df5a1f0c1b
Heorhii Huziuk <hh53872>
2023-11-15 20:02:00 +0100

    added realization for 5 laboratory

---
+++ Laboratory1/lab5/lab5/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(145, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 46);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "first name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(145, 133);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 46);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "last name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(145, 198);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 46);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "birth date";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(145, 267);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(145, 311);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(400, 49);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(387, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 46);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Laboratory1/lab5/lab5/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<Person> people = new BindingList<Person>();
+        private BindingSource bindingSource = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBindings();
+        }
+
+        private void InitializeBindings()
+        {
+            bindingSource.DataSource = people;
+            listBox1.DataSource = bindingSource;
+            listBox1.DisplayMember = "FullName";
+            comboBox1.Items.AddRange(new[] { "Male", "Female" });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstName = textBox1.Text;
+            string lastName = textBox2.Text;
+            string birthDate = textBox3.Text;
+            string gender = comboBox1.SelectedItem as string;
+
+            if (TryGetPersonFromInput(firstName, lastName, birthDate, gender, out Person newPerson))
+            {
+                people.Add(newPerson);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                people.RemoveAt(selectedIndex);
+            }
+            else
+            {
+                ShowErrorMessage("Select a person to delete.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                if (TryGetPersonFromInput(textBox1.Text, textBox2.Text, textBox3.Text, comboBox1.SelectedItem as string, out Person updatedPerson))
+                {
+                    people[selectedIndex] = updatedPerson;
+                }
+            }
+            else
+            {
+                ShowErrorMessage("Select a person to update.");
+            }
+        }
+
+        private bool TryGetPersonFromInput(string firstName, string lastName, string birthDate, string gender, out Person person)
+        {
+            if (ValidateInput(firstName, lastName, birthDate, gender) && DateTime.TryParse(birthDate, out DateTime birthDateTime))
+            {
+                person = new Person(firstName, lastName, birthDateTime, gender);
+                return true;
+            }
+
+            person = null;
+            ShowErrorMessage("Invalid input. Please check all fields.");
+            return false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                DisplayPersonData(people[selectedIndex]);
+            }
+        }
+
+        private void DisplayPersonData(Person person)
+        {
+            textBox1.Text = person.FirstName;
+            textBox2.Text = person.LastName;
+            textBox3.Text = person.BirthDate.ToString("yyyy-MM-dd");
+            comboBox1.SelectedItem = person.Gender;
+        }
+
+        private bool ValidateInput(params string[] values)
+        {
+            foreach (string value in values)
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public DateTime BirthDate { get; }
+        public string Gender { get; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public Person(string firstName, string lastName, DateTime birthDate, string gender)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Gender = gender;
+        }
+    }
+}
