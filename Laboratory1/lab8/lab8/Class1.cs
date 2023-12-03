6a1ac38fac6c90be3b33756146916204e4817889
Heorhii Huziuk <hh53872>
2023-12-03 12:49:16 +0100

    added 07 and 08 laboratory

---
+++ Laboratory1/lab8/lab8/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    public class CustomTimer
+    {
+        private readonly int interval;
+        private bool isRunning;
+        private CancellationTokenSource cts;
+
+        public event Action timeElapsed;
+        public event Action<Exception> onError;
+
+        public CustomTimer(int interval)
+        {
+            this.interval = interval;
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                cts = new CancellationTokenSource();
+
+                Task.Run(() => TimerLoop(cts.Token), cts.Token);
+            }
+        }
+
+        public void Stop()
+        {
+            if (isRunning)
+            {
+                cts.Cancel();
+                isRunning = false;
+            }
+        }
+
+        private async void TimerLoop(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(interval, cancellationToken);
+                    OnTimeElapsed();
+                }
+            }
+            catch (Exception ex)
+            {
+                OnError(ex);
+            }
+        }
+
+        private void OnTimeElapsed()
+        {
+            if (timeElapsed != null)
+            {
+                try
+                {
+                    timeElapsed.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    OnError(ex);
+                }
+            }
+        }
+
+        private void OnError(Exception ex)
+        {
+            if (onError != null)
+            {
+                onError.Invoke(ex);
+            }
+        }
+    }
+}
---
+++ Laboratory1/lab8/lab8/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8
+{
+    public partial class Form1 : Form
+    {
+        private CustomTimer customTimer;
+        public Form1()
+        {
+            InitializeComponent();
+            customTimer = new CustomTimer(10000); 
+            customTimer.timeElapsed += CustomTimer_TimeElapsed;
+            customTimer.onError += CustomTimer_OnError;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            customTimer.Start();
+        }
+
+        private void CustomTimer_TimeElapsed()
+        {
+            Invoke(new Action(() =>
+            {
+                textBox1.AppendText("Time Elapsed!\r\n");
+            }));
+        }
+
+        private void CustomTimer_OnError(Exception ex)
+        {
+            Invoke(new Action(() =>
+            {
+                textBox1.AppendText($"Error: {ex.Message}\r\n");
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customTimer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            customTimer.Stop();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            customTimer.Stop();
+        }
+    }
+}
