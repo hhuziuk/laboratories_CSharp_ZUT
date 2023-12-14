210e8a99f511a31dcfb71f0500c7b4ea2169cec6
Heorhii Huziuk <hh53872>
2023-12-14 23:10:31 +0100

    09lab

---
+++ Laboratory1/09Aaaaaaa/09Aaaaaaa/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CMH;
+
+namespace lab8
+{
+    public partial class Form1 : Form
+    {
+        private CustomTimer customTimer;
+        private int matrixSize = 500;
+        private int[,] matrixA;
+        private int[,] matrixB;
+        private int[,] resultMatrix;
+
+        public Form1()
+        {
+            InitializeComponent();
+            customTimer = new CustomTimer(5000);
+            customTimer.timeElapsed += TimerElapsedHandler;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            customTimer.Start();
+        }
+
+        private void TimerElapsedHandler()
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            customTimer.Stop();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var customBackgroundWorker = new CustomBackgroundWorker();
+            customBackgroundWorker.ProgressChanged += MatrixWorker_ProgressChanged;
+            customBackgroundWorker.Error += MatrixWorker_Error;
+
+            InitializeMatrices();
+
+            await Task.Run(() => MultiplyMatrices(matrixA, matrixB, resultMatrix));
+
+
+
+            MessageBox.Show("Matrix Multiplication was done");
+        }
+
+        private void MatrixWorker_ProgressChanged(int progressPercentage)
+        {
+            progressBar1.Invoke((MethodInvoker)(() => progressBar1.Value = progressPercentage));
+        }
+
+        private void MatrixWorker_Error(Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void InitializeMatrices()
+        {
+            matrixA = new int[matrixSize, matrixSize];
+            matrixB = new int[matrixSize, matrixSize];
+            resultMatrix = new int[matrixSize, matrixSize];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    matrixA[i, j] = rand.Next(1, 10);
+                    matrixB[i, j] = rand.Next(1, 10);
+                }
+            }
+        }
+
+        private void MultiplyMatrices(int[,] matrixA, int[,] matrixB, int[,] resultMatrix)
+        {
+            int matrixSize = matrixA.GetLength(0);
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++)
+                    {
+                        sum += matrixA[i, k] * matrixB[k, j];
+                    }
+                    resultMatrix[i, j] = sum;
+
+                    int progressPercentage = (int)(((i * matrixSize) + j) / (double)(matrixSize * matrixSize) * 100);
+                    MatrixWorker_ProgressChanged(progressPercentage);
+                }
+            }
+        }
+    }
+}
---
+++ Laboratory1/09Aaaaaaa/CMH/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CMH
+{
+    public class CustomTimer
+    {
+        private readonly int interval;
+        private bool isRunning;
+        private CancellationTokenSource cts;
+
+        public event Action timeElapsed;
+        public event Action<Exception> onError;
+
+        public CustomTimer(int interval)
+        {
+            this.interval = interval;
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                cts = new CancellationTokenSource();
+
+                ThreadPool.QueueUserWorkItem(state => TimerLoop(cts.Token));
+            }
+        }
+
+        public void Stop()
+        {
+            if (isRunning)
+            {
+                cts.Cancel();
+                isRunning = false;
+            }
+        }
+
+        private void TimerLoop(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Thread.Sleep(interval);
+                    OnTimeElapsed();
+                }
+            }
+            catch (Exception ex)
+            {
+                OnError(ex);
+            }
+        }
+
+        private void OnTimeElapsed()
+        {
+            timeElapsed?.Invoke();
+        }
+
+        private void OnError(Exception ex)
+        {
+            onError?.Invoke(ex);
+        }
+    }
+
+    public class CustomBackgroundWorker
+    {
+        private Task workerTask;
+
+        public event Action<int> ProgressChanged;
+        public event Action<Exception> Error;
+
+        public void RunWorkerAsync(object argument)
+        {
+            workerTask = Task.Run(() => DoWork(argument));
+        }
+
+        public void ReportProgress(int progressPercentage)
+        {
+            ProgressChanged?.Invoke(progressPercentage);
+        }
+
+        private void DoWork(object argument)
+        {
+            try
+            {
+                for (int i = 0; i <= 100; i += 10)
+                {
+                    ReportProgress(i);
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Error?.Invoke(ex);
+            }
+        }
+    }
+}
